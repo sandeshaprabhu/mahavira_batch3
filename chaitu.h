c82b8a5e6045e02c4760903af6373bea6ceb60a5
stepin 105063 <69027657+stepin-105053@users.noreply.github.com>
2021-02-04 15:43:34 +0530

    Create chaitu.h
    added header

---
+++ chaitu.h
@@ -0,0 +1,12 @@
+#ifndef NAGACHAITHANYA_H
+#define NAGACHAITHANYA_H
+#include<stdio.h>
+//FUNCTION DECLARATION
+
+void printnagachaithanya()
+{
+    printf("I am nagachaithanya k \n");
+}
+
+
+#endif
